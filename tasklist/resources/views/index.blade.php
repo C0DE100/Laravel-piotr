aec574553964f33f829d82fb2ba5a47d4f36ccd6
Aleksandar Jovanovski <jovanovski.a74@gmail.com>
2026-08-08 18:18:39 +0200

    project 1 finished

--- tasklist/resources/views/index.blade.php
+++ tasklist/resources/views/index.blade.php
@@ -1,13 +1,25 @@
+@extends('layouts.app')
 
-<h1>Tasks</h1>
 
-<div>
+@section('title', 'The list of tasks')
+
+@section('content')
+    <nav class="mb-4">
+        <a class="link" 
+            href="{{ route('tasks.create') }}">
+            Add task
+        </a>
+    </nav>
     @forelse ($tasks as $task)
         <div> 
-            {{-- <a href="{{  route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a> --}}
-            {{dd(route('tasks.show', ['id' => $task->id]))}}
+            <a href="{{  route('tasks.show', ['task' => $task->id]) }}"
+                @class(['line-through' => $task->completed])>{{ $task->title }}</a>
         </div>
     @empty
         <div>No tasks</div>
     @endforelse
-</div>
+    
+    @if ($tasks->count())
+        <nav class="mt-4">{{ $tasks->links() }}</nav>
+    @endif
+@endsection
